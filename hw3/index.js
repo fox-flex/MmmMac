e7e0881ef1bc4d602b24a26c8c038cffba44062f
Mykhailo Pasichnyk <michaelpasichnyk12@gmail.com>
2022-06-16 05:24:00 +0300

    some done

--- hw3/index.js
+++ hw3/index.js
@@ -1,14 +1,82 @@
 /* YOUR CODE HERE! */
-let boxes = document.getElementsByClassName("box");
-const container = document.querySelector(".box-container");
+// let boxes = document.getElementsByClassName("box");
+const boxContainer = document.querySelector(".box-container");
+const container = document.getElementsByClassName('box-container')[0]
 
 let BOX_ID = 2;
 
-buildCoolBox(boxes[0]);
+function getNumOfBoxes() {
+    return document.getElementsByClassName('box').length;
+}
+let runDrag = false;
+let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+
+container.addEventListener('mousedown', (ev) => {
+    console.log(`num boxes  - ${getNumOfBoxes()}`)
+    let box = ev.target;
+
+    if (box && box.className === 'box' || box.className === 'box box-large') {
+        if (ev.button === 0) { // left click
+            if (ev.shiftKey) {
+                resizeHandler(box);
+            } else if (ev.ctrlKey) {     // for debug on touchpad
+                changeColorHandler(box); // for debug
+            } else {
+                runDrag = true;
+            }
+        // } else if (ev.button === 2) { // right click
+        //         changeColorHandler(box);
+        }
+    }
+})
+
+container.addEventListener('onmouseup', (ev) => {
+    if (runDrag) {
+        pos1 = 0; pos2 = 0; pos3 = 0; pos4 = 0;
+        runDrag = false;
+    }
+})
+
+container.addEventListener('onmousemove', (ev) => {
+    let box = ev.target;
+
+    if (runDrag && box && box.className === 'box' || box.className === 'box box-large') {
+        if (ev.button === 0) { // left click
+            move(box);
+        }
+    }
+})
+
+container.addEventListener('dblclick', (ev) => {
+    let box = ev.target;
+    console.log('1');
+    if (box && box.className === 'box' || box.className === 'box box-large') {
+        console.log('2');
+        if (ev.button === 0) { // left click
+            if (ev.altKey) {
+                eraseHandler(box);
+            } else if (!ev.shiftKey && !ev.shiftKey && !ev.ctrlKey) {
+                duplicateHandler(box);
+            }
+        }
+    }
+})
 
 
 function resizeHandler(el) {
+    // el.toggle('box-large');
     el.classList.toggle('box-large');
+}
+
+function changeColorHandler(el) {
+    const colors = ['blue', 'yellow', 'red', 'black'];
+    const colNum = colors.length;
+
+    let ind_new = Math.floor(Math.random() * colNum);
+    if (el.style.background === colors[ind_new]) {
+        ind_new = (ind_new + 1) % colNum;
+    }
+    el.style.backgroundColor = colors[ind_new];
 }
 
 function duplicateHandler(el) {
@@ -18,87 +86,25 @@ function duplicateHandler(el) {
     console.log(el.offsetTop + el.clientHeight + 'px')
     new_el.style.top = el.offsetTop + el.clientHeight + 'px';
     new_el.style.left = el.offsetLeft + el.clientWidth + 'px';
-    container.appendChild(new_el);
-    buildCoolBox(new_el);
-    boxes = document.getElementsByClassName("box");
-}
-
-function changeColorHandler(el) {
-    const colors = ['blue', 'yellow', 'red', 'black'];
-    const colNum = colors.length;
-
-    let ind_new = Math.floor( Math.random() * colNum);
-    if (el.style.background === colors[ind_new]) {
-        ind_new = (ind_new+1) % colNum;
-    }
-    el.style.backgroundColor = colors[ind_new];
+    boxContainer.appendChild(new_el);
+    // buildCoolBox(new_el);
+    // boxes = document.getElementsByClassName("box");
 }
 
 function eraseHandler(el) {
-    if(boxes.length > 1) {
+    if (getNumOfBoxes() > 1) {
         el.parentNode.removeChild(el);
-        boxes = document.getElementsByClassName("box");
     }
 }
 
-/* src: https://www.w3schools.com/howto/howto_js_draggable.asp */
-let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-function dragHandler(el) {
-    el.onmousedown = dragMouseDown;
-
-    function dragMouseDown(el) {
-        el = el || window;
-        el.preventDefault();
-        // get the mouse cursor position at startup:
-        pos3 = el.clientX;
-        pos4 = el.clientY;
-        el.onmouseup = closeDragElement;
-        // call a function whenever the cursor moves:
-        el.onmousemove = elementDrag;
-      }
-
-    function elementDrag(el) {
-        el = el || window;
-        el.preventDefault();
-        // calculate the new cursor position:
-        pos1 = pos3 - el.clientX;
-        pos2 = pos4 - el.clientY;
-        pos3 = el.clientX;
-        pos4 = el.clientY;
-        // set the element's new position:
-        el.style.top = (el.offsetTop - pos2) + "px";
-        el.style.left = (el.offsetLeft - pos1) + "px";
-    }
-
-    function closeDragElement(el) {
-        // stop moving when mouse button is released:
-        el.onmouseup = null;
-        el.onmousemove = null;
-    }
+function move(el) {
+    // calculate the new cursor position:
+    pos1 = pos3 - el.clientX;
+    pos2 = pos4 - el.clientY;
+    pos3 = el.clientX;
+    pos4 = el.clientY;
+    // set the element's new position:
+    el.style.top = (el.offsetTop - pos2) + "px";
+    el.style.left = (el.offsetLeft - pos1) + "px";
 }
 
-function buildCoolBox(box) {
-    box.addEventListener('mousedown', (ev) => {
-        if (ev.button === 0) { // left click
-            if (ev.shiftKey) {
-                resizeHandler(box);
-            } else {
-                // pos1 = 0; pos2 = 0; pos3 = 0; pos4 = 0;
-                dragHandler(box);
-            }
-        } else if (ev.button === 2) { // right click
-        // } if (ev.ctrlKey) { // for debug on touchpad
-            changeColorHandler(box);
-        }
-    });
-
-    box.addEventListener('dblclick', (ev) => {
-        if (ev.button === 0) { // left click
-            if (ev.altKey) {
-                eraseHandler(box);
-            } else if (!ev.shiftKey && !ev.shiftKey && !ev.ctrlKey) {
-                duplicateHandler(box);
-            }
-        }
-    })
-}
