ef8cac2c6635e0c54b9af37395bfffdcda8a5231
Mykhailo Pasichnyk <michaelpasichnyk12@gmail.com>
2022-06-16 14:21:14 +0300

    done

--- hw3/index.js
+++ hw3/index.js
@@ -1,18 +1,20 @@
 /* YOUR CODE HERE! */
-let boxes = document.getElementsByClassName("box");
-const boxContainer = document.querySelector(".box-container");
+let boxes = document.getElementsByClassName('box');
+const boxContainer = document.querySelector('.box-container');
 const container = document.getElementsByClassName('box-container')[0]
 
 let BOX_ID = 2;
+window.addEventListener("contextmenu", e => e.preventDefault());
 
-// [...boxes].forEach( box => { addDrag(box); });
-[...boxes].forEach( box => { addDrag(box); });
+[...boxes].forEach(box => {
+    addDrag(box);
+});
 
 function getNumOfBoxes() {
     return document.getElementsByClassName('box').length;
 }
 
-function doForBoxes(type, func) {
+function decoratorToAddListenerForAllBoxes(type, func) {
     container.addEventListener(type, (ev) => {
         let box = ev.target
         if (box && box.className === 'box'
@@ -22,17 +24,17 @@ function doForBoxes(type, func) {
     });
 }
 
-doForBoxes('contextmenu', (ev, box) => {
+decoratorToAddListenerForAllBoxes('contextmenu', (ev, box) => {
     changeColorHandler(box);
 });
 
-doForBoxes('click', (ev, box) => {
+decoratorToAddListenerForAllBoxes('click', (ev, box) => {
     if (ev.shiftKey) {
         resizeHandler(box);
     }
 });
 
-doForBoxes('dblclick', (ev, box) => {
+decoratorToAddListenerForAllBoxes('dblclick', (ev, box) => {
     if (ev.button === 0) { // left click
         if (ev.altKey) {
             eraseHandler(box);
@@ -50,22 +52,21 @@ function changeColorHandler(el) {
     const colors = ['blue', 'yellow', 'red', 'black'];
     const colNum = colors.length;
 
-    let ind_new = Math.floor(Math.random() * colNum);
-    if (el.style.background === colors[ind_new]) {
-        ind_new = (ind_new + 1) % colNum;
+    let indNew = Math.floor(Math.random() * colNum);
+    if (el.style.background === colors[indNew]) {
+        indNew = (indNew + 1) % colNum;
     }
-    el.style.backgroundColor = colors[ind_new];
+    el.style.backgroundColor = colors[indNew];
 }
 
 function duplicateHandler(el) {
-    let new_el = el.cloneNode(true);
-    new_el.textContent = BOX_ID;
+    let newEl = el.cloneNode(true);
+    newEl.textContent = BOX_ID;
     BOX_ID += 1;
-    console.log(el.offsetTop + el.clientHeight + 'px')
-    new_el.style.top = el.offsetTop + el.clientHeight + 'px';
-    new_el.style.left = el.offsetLeft + el.clientWidth + 'px';
-    boxContainer.appendChild(new_el);
-    addDrag(new_el);
+    newEl.style.top = el.offsetTop + el.clientHeight + 'px';
+    newEl.style.left = el.offsetLeft + el.clientWidth + 'px';
+    boxContainer.appendChild(newEl);
+    addDrag(newEl);
 }
 
 function eraseHandler(el) {
@@ -74,19 +75,17 @@ function eraseHandler(el) {
     }
 }
 
-function addDrag (element) {
+function addDrag(element) {
     let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-    element.onmousedown = dragMouseDown;
-    // element.addEventListener('onmousedown', dragMouseDown);
+    element.addEventListener('mousedown', dragMouseDown, true);
 
-    function dragMouseDown (ev) {
+    function dragMouseDown(ev) {
         ev = ev || window;
-        ev.preventDefault();
         // get the mouse cursor position at startup:
         pos3 = ev.clientX;
         pos4 = ev.clientY;
-        document.onmouseup = closeDragElement;
-        document.onmousemove = elementDrag;
+        document.addEventListener('mouseup', closeDragElement);
+        document.addEventListener('mousemove', elementDrag);
     }
 
     const elementDrag = (ev) => {
@@ -98,13 +97,13 @@ function addDrag (element) {
         pos3 = ev.clientX;
         pos4 = ev.clientY;
         // set the element's new position:
-        element.style.top = (element.offsetTop - pos2) + "px";
-        element.style.left = (element.offsetLeft - pos1) + "px";
+        element.style.top = (element.offsetTop - pos2) + 'px';
+        element.style.left = (element.offsetLeft - pos1) + 'px';
     }
 
     const closeDragElement = () => {
         // stop moving when mouse button is released:
-        document.onmouseup = null;
-        document.onmousemove = null;
+        document.removeEventListener('mouseup', closeDragElement);
+        document.removeEventListener('mousemove', elementDrag);
     }
 }
